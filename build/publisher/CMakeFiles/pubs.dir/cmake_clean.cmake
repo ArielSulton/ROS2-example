ce7cc565f68c4f9f99d263d155b04630dd20607b
ArielSulton <arielsulton26@gmail.com>
2025-01-10 14:37:12 +0700

    git commit

---
+++ build/publisher/CMakeFiles/pubs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pubs.dir/src/pubs.cpp.o"
+  "pubs"
+  "pubs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pubs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/launcher/launch/test.launch.py
@@ -0,0 +1,20 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package="publisher",
+            executable="pubs",
+            name="pubs",
+            output="screen",
+            emulate_tty=True,
+        ),
+        Node(
+            package="subscriber",
+            executable="subs",
+            name="subs",
+            output="screen",
+            emulate_tty=True,
+        )
+    ])
